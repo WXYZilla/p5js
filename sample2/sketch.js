b08dabbcd43c6c386d181cf9b9dfcb84f12befe3
WXYZilla <chompzilla07@gmail.com>
2026-04-21 11:18:08 +0900

    Update sketch.js

--- sample2/sketch.js
+++ sample2/sketch.js
@@ -2,11 +2,13 @@ let padX, padY, padWidth;
 let xPos, xDir, yPos, yDir, diam, speed;
 let gameStart = 1;
 let gameOver = 0;
-let randomColor1, randomColor2, randomN, bounced = 1;
+let randomColor1, randomColor2, bounced = 1;
+let t = 0;
+let bricks = [1,1,1,1,1];
 
 function setup() {
-  createCanvas(600,800);
-  speed = 5;
+  createCanvas(1000,1200);
+  speed = 12;
   xPos = width / 2;
   xDir = -speed;
   yPos = (height - 200) / 2;
@@ -14,32 +16,61 @@ function setup() {
   diam = 50;
   padWidth = 200;
   randomColor1 = color('#FF0000');
+  randomColor2 = colorGenerate();
 }
 
 function draw() {
+  //background
   background(128);
+  
+  //drawing lower part
   fill(200);
   noStroke();
-  rect(0,600,width,200);
+  rect(0,height-200,width,200);
+  fill(randomColor2);
+  circle(100, height - 200 + 100, 70);
   noFill();
   stroke(255);
   square(40,height - 200 + 40 , 120);
   
+  //drawing ball
   fill(200);
   stroke(randomColor1);
   strokeWeight(10);
   circle(xPos,yPos,diam);
   if(gameOver == 0){
-  xPos = xPos + xDir;
-  yPos = yPos + yDir;
+    xPos = xPos + xDir;
+    yPos = yPos + yDir;
   }
   
+  //drawing the bounce pad
   padX = mouseX - padWidth/2;
   padY = height - 230;
   fill(0);
   noStroke();
   rect(padX, padY, padWidth, 30);
   
+  //baounce pad hitbox
+  hitbox();
+  
+  //changing ball color
+  if(yDir == -speed && bounced == 1 && gameOver != 1){
+    bounced = 0;
+    gameStart = 0;
+    randomColor1 = randomColor2;
+    randomColor2 = colorGenerate();
+  }
+  
+  
+  
+  if(t % 1000 == 0){
+    summonBricks();
+  }
+  
+  timer();
+}
+
+function hitbox() {
   if(xPos - diam/2 - 10 < 0 || xPos + diam/2 + 10 > width) xDir *= -1;
   if(yPos + diam/2 + 10 > height - 200) gameOver = 1;
   if(yPos - diam/2 - 10 < 0) yDir = speed;
@@ -57,27 +88,58 @@ function draw() {
     xDir = abs(xDir);
     bounced = 1;
   }
-    if(yDir == -speed && bounced == 1){
-    bounced = 0;
-    gameStart = 0;
-    randomN = Math.floor(random(16));
+}
+
+function colorGenerate() {
+  let randomN = Math.floor(random(16));
     if(randomN >= 1 && randomN <= 3){
-      randomColor1 = ('#FF0000');
+      return color('#FF0000');
     }
     if(randomN >= 4 && randomN <= 6){
-      randomColor1 = ('#FFFF00');
+      return color('#FFFF00');
     }
     if(randomN >= 7 && randomN <= 9){
-      randomColor1 = ('#0000FF');
+      return color('#0000FF');
     }
     if(randomN >= 10 && randomN <= 12){
-      randomColor1 = ('#00FF00');
+      return color('#00FF00');
     }
     if(randomN >= 13 && randomN <= 15){
-      randomColor1 = ('#FF00FF');
+      return color('#FF00FF');
     }
     if(randomN == 0){
-      randomColor1 = ('#000000');
+      return color('#000000');
+    }
+}
+
+function summonBricks() {
+  let randomN;
+  let i = 0;
+  while(i < bricks.length){
+    if ( bricks[i] === ) {
+      randomN = Math.floor(random(1,6));
+      if(randomN >= 1 && randomN <= 3){
+        fill('#FF0000');
+      }
+      if(randomN >= 4 && randomN <= 6){
+        return color('#FFFF00');
+      }
+      if(randomN >= 7 && randomN <= 9){
+        return color('#0000FF');
+      }
+      if(randomN >= 10 && randomN <= 12){
+        return color('#00FF00');
+      }
+      if(randomN >= 13 && randomN <= 15){
+        return color('#FF00FF');
+      }
+      noStroke();
+      rect(i*180+20, 0, 180,100);
+      i++;
     }
   }
+}
+
+function timer() {
+  t++;
 }
